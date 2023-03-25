5c425dec634c65f97be6eda96639874fdf153d7c
Joao Vittor <Akiraseira@hotmail.com>
2023-03-25 17:52:21 -0300

    day 3

--- day3.py
+++ day3.py
@@ -1,3 +1,8 @@
 #Day where im working with variables, if, else and making of multiple choice at the end
-
-
+#Little program to check if the number is odd or even
+number = int(input("Type the number that will be checked : "))
+if number % 2 == 0:
+    print(f"the {number} that you typed is even")
+else:
+    print(f"The {number} that you typed is odds")
+    
