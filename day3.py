211d2899939df5c2a4c7d6e247ee0f02dba5737a
Joao Vittor <Akiraseira@hotmail.com>
2023-03-25 21:07:56 -0300

    Commit nested if statement

--- day3.py
+++ day3.py
@@ -6,3 +6,14 @@ if number % 2 == 0:
 else:
     print(f"The {number} that you typed is odds")
     
+##nested If statment for fun
+name = input("What is your name ?\n")
+age = int(input("What is your age ? \n"))
+height = float(input("What is your Height in centimeteres ? \n"))
+if height > 120:
+    if age > 15:
+        print(f"Your {name} are qualified")
+    else:
+        print(f"Due to your age being {age} you are not qualified")
+else:
+    print(f"Due to your height of {round(height)} centimeters you are not qualified")    
