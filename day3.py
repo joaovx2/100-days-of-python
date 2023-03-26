c4f82134e1d549220622b35dfc72a8adea2485da
Joao Vittor <Akiraseira@hotmail.com>
2023-03-26 14:42:55 -0300

    Close to finishing off the day

--- day3.py
+++ day3.py
@@ -17,3 +17,27 @@ if height > 120:
         print(f"Due to your age being {age} you are not qualified")
 else:
     print(f"Due to your height of {round(height)} centimeters you are not qualified")    
+    
+    
+#elif stuff
+height_bmi = input("Enter your Height in Meters : \n")
+weight_bmi = input("Enter your Weight in KG : \n")
+body_mass_bmi = round(float(weight_bmi) / float(height_bmi) ** 2)
+if body_mass_bmi > 35:
+    print(f"Your body mass is {body_mass_bmi} you're clinically obese.")
+elif body_mass_bmi >=30:
+    print(f"Your body mass is {body_mass_bmi} you're obese.")
+elif body_mass_bmi >= 25:
+    print(f"Your body mass is {body_mass_bmi} you're overweight.")
+elif body_mass_bmi >= 18.5:
+    print(f"Your body mass is {body_mass_bmi} you have normal weight.")
+else:
+    print(f"Your body mass is {(body_mass_bmi)} you are underweight.")
+    
+##checking if the year is a leap year
+year = int(input("What is the year you want to check ? "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
+       
