14fdeda576d1362737298238295eeb9e1780b9f9
Joao Vittor <Akiraseira@hotmail.com>
2023-03-26 14:43:07 -0300

    Close to finishing off the day

--- day3.py
+++ day3.py
@@ -40,4 +40,37 @@ if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print(year, "is a leap year")
 else:
     print(year, "is not a leap year")
+    
+##checking with IF statements for practice
+year = int(input("What is the year you want to check ? "))
+if (year % 4 == 0):
+    if (year % 100 ==0):
+        if( year % 400 ==0):
+            print(f"The year {year} is a leap year")
+        else:
+            print(f"The year {year}, is not a leap year.")
+    else:
+        print(f"The year {year}, is a leap year")
+else:
+    print(f"The year {year}, is not a leap year.")
        
+##basic logic of IF statements
+#IF condition A = true
+#DOES A
+#IF Condition B = ALSO TRUE
+#Does B
+#IF Condition C = ALSO TRUE
+#Do C
+#For nested IF, all the conditions need to be true for the code to be executed, in this case to execute C, A and B need to be true
+#
+######################################
+#Basic of IF but with ELIF 
+#If condition A = TRUE
+#DOES A
+#ELIF condition B = TRUE
+#DOES B, in this case, a doesnt need to be true to execute B
+#ELSE
+#DOES C
+#in this case only one of the conditions need to be true, it will check if A is true, if its false it will go to be, if its false it will go to C
+
+
