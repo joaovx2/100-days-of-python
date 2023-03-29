c35ea8765201239da55e9454fd28ea1c4802fe9b
Joao Vittor <Akiraseira@hotmail.com>
2023-03-28 23:03:05 -0300

    piroca

--- day7_hangman.py
+++ day7_hangman.py
@@ -1,4 +1,61 @@
 ##day about loops and functions
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+lives = 6
 choosen_word = input("Type the word that the user will be guessing : ").lower()
 print(f"Choosen word is {choosen_word}")
 display = []
@@ -8,13 +65,14 @@ print(display)
 game_over = False
 while  game_over == False: ##while NOT game_over: also works the same
     guess_letter = input("Start guessing letters from the choosen word : ").lower()#keeps asking for a new letter until game is over
-    for position in range(len(choosen_word)): #gets the positions in the range of the choosen word
+    for position in range(len(choosen_word)): #the first loop that will go through each letter  of the choosen word
         letter = choosen_word[position] 
-        if letter == guess_letter: #this replaces the display of the choosen word with the right letter if it the case
-            display[position] = letter 
-    print(display) ##displays the chosen word list with the letter replacing
+        if letter == guess_letter: #this checks if the letter is equal to the guess
+            display[position] = letter  #replaces the blanket position with the letter
+    print(f"{''.join(display)}") ##displays the chosen word list with the letter replacing
     if "_" not in display: ##checks if there is empty blanekts
         game_over = True #if there no more blanket spaces
         print("Game Over.") #show that its game over
+        
 
 
