a69f00dd2c1c03d466dccd9d38bfb1e8594345b5
Joao Vittor <Akiraseira@hotmail.com>
2023-03-28 23:36:20 -0300

    finishing up hangman

--- day7_hangman.py
+++ day7_hangman.py
@@ -1,6 +1,7 @@
 ##day about loops and functions
 import day7_hangman_art
 lives = 6
+print(day7_hangman_art.logo)
 choosen_word = input("Type the word that the user will be guessing : ").lower()
 print(f"Choosen word is {choosen_word}")
 display = []
@@ -10,6 +11,8 @@ print(display)
 game_over = False
 while  game_over == False: ##while NOT game_over: also works the same
     guess_letter = input("Start guessing letters from the choosen word : ").lower()#keeps asking for a new letter until game is over
+    if guess_letter in display:
+        print(f"The letter {guess_letter.upper()} was already guessed")
     for position in range(len(choosen_word)): #the first loop that will go through each letter  of the choosen word
         letter = choosen_word[position] 
         if letter == guess_letter: #this checks if the letter is equal to the guess
@@ -17,7 +20,7 @@ while  game_over == False: ##while NOT game_over: also works the same
     print(f"{''.join(display)}") ##displays the chosen word list with the letter replacing
     if guess_letter  not in choosen_word: ##checks if the letter is NOT in the choosen word
         lives -= 1 ##subtracts a life
-        print("Your guess didnt match, you lost a live.") #tells you, you lost a life
+        print(f"You already tried the letter {guess_letter.upper()} and it's not in the word") #tells you, you lost a life
         print(day7_hangman_art.stages[lives])#prints the asci art for the stages
         if lives == 0: #when the life is = 0 
             print("Game Over! You died.") #shows you lost the game
