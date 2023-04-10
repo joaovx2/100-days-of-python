1594a9af7e8dfdd61e84fb9e1c6f7710bce861fc
Joao Vittor <Akiraseira@hotmail.com>
2023-04-10 14:24:25 -0300

    Coffe_machine_Finished

--- day14/day14.py
+++ day14/day14.py
@@ -1,3 +1,14 @@
-import random
-number = random.randint(0 , 999999999999)
-print(number)
+#interview question, returning a string as uppercase, lower case etc
+
+
+def upper_lower(string):
+    new_string = ''
+    for i in range(len(string)):
+        if i % 2 == 0:
+            new_string += string[i].upper()
+        else:
+            new_string += string[i].lower()
+    return new_string
+user_input = input("Enter a string: ")
+result = upper_lower(user_input)
+print(result)
